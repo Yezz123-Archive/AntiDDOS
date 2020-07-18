769b0fa16b1c4cad5da9768e1b5decc655ea4974
Yasser Tahiri <yasserth19@gmail.com>
2020-07-18 10:59:58 +0100

    Add files via upload

---
+++ ML/gaussian_naive_bayes.py
@@ -0,0 +1,39 @@
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+
+def main():
+
+
+    # Load Iris dataset
+    iris = load_iris()
+
+    # Split dataset into train and test data
+    X = iris["data"]  # features
+    Y = iris["target"]
+    x_train, x_test, y_train, y_test = train_test_split(
+        X, Y, test_size=0.3, random_state=1
+    )
+
+    # Gaussian Naive Bayes
+    NB_model = GaussianNB()
+    NB_model.fit(x_train, y_train)
+
+    # Display Confusion Matrix
+    plot_confusion_matrix(
+        NB_model,
+        x_test,
+        y_test,
+        display_labels=iris["target_names"],
+        cmap="Blues",
+        normalize="true",
+    )
+    plt.title("Normalized Confusion Matrix - IRIS Dataset")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
